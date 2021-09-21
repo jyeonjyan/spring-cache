826377a5778698c1543ac244adcd57afdc929dbb
johnjihwan <s20062@gsm.hs.kr>
2021-09-21 15:34:26 +0900

    Add 간단한 save 로직

--- src/main/java/com/cache/spring/service/BookService.java
+++ src/main/java/com/cache/spring/service/BookService.java
@@ -1,2 +1,17 @@
-package com.cache.spring.service;public class BookService {
+package com.cache.spring.service;
+
+import com.cache.spring.dto.BookDto;
+import com.cache.spring.entity.BookEntity;
+import com.cache.spring.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookEntity saveBook(BookDto bookDto){
+        return bookRepository.save(bookDto.toEntity());
+    }
 }
