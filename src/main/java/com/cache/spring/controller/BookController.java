25b1a917e450f9cb9879e56a19425a693fd0709f
johnjihwan <s20062@gsm.hs.kr>
2021-09-21 15:35:43 +0900

    Add 간단한 controller 작성

--- src/main/java/com/cache/spring/controller/BookController.java
+++ src/main/java/com/cache/spring/controller/BookController.java
@@ -1,14 +1,24 @@
 package com.cache.spring.controller;
 
+import com.cache.spring.dto.BookDto;
+import com.cache.spring.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class BookController {
+    private final BookService bookService;
+
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello springboot";
+    }
+
+    @PostMapping("/save")
+    public void saveBook(BookDto bookDto){
+        bookService.saveBook(bookDto);
     }
 }
