02beafe4345d42c4e1446976cef935801a5599bc
johnjihwan <s20062@gsm.hs.kr>
2021-09-21 15:47:18 +0900

    Add swagger 설정 완료

--- src/main/java/com/cache/spring/controller/BookController.java
+++ src/main/java/com/cache/spring/controller/BookController.java
@@ -2,6 +2,7 @@ package com.cache.spring.controller;
 
 import com.cache.spring.dto.BookDto;
 import com.cache.spring.service.BookService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
     private final BookService bookService;
 
+    @ApiOperation("서버 상태 확인하기")
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello springboot";
     }
 
+    @ApiOperation("책 추가하기")
     @PostMapping("/save")
     public void saveBook(BookDto bookDto){
         bookService.saveBook(bookDto);
