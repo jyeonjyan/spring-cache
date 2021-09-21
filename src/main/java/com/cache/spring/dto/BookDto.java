ab421a1f795f792f7b37866058fa9d3aa336a3be
johnjihwan <s20062@gsm.hs.kr>
2021-09-21 15:32:15 +0900

    Add BookDto

---
+++ src/main/java/com/cache/spring/dto/BookDto.java
@@ -0,0 +1,22 @@
+package com.cache.spring.dto;
+
+import com.cache.spring.entity.BookEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDto {
+    private String author;
+    private String publisher;
+
+    public BookEntity toEntity(){
+        return BookEntity.builder()
+                .author(this.author)
+                .publisher(this.publisher)
+                .build();
+    }
+}
---
+++ src/main/java/com/cache/spring/service/BookService.java
@@ -0,0 +1,2 @@
+package com.cache.spring.service;public class BookService {
+}
