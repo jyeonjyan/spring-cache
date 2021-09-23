b3c7629b8bcdd872e8da7ae6295a46e837822b9a
johnjihwan <s20062@gsm.hs.kr>
2021-09-23 14:17:56 +0900

    Rollback 굳이 필요 없을 것 같아

--- src/main/java/com/cache/spring/repository/BookRepository.java
+++ src/main/java/com/cache/spring/repository/BookRepository.java
@@ -2,8 +2,6 @@ package com.cache.spring.repository;
 
 import com.cache.spring.entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface BookRepository extends JpaRepository<BookEntity, Long>, BookRepositoryCustom{
+public interface BookRepository extends JpaRepository<BookEntity, Long>{
 }
